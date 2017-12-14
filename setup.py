cc441457833447c37866899663a12447ef0293a8
Andy Yaco-Mink <ayacomink@etsy.com>
2017-12-14 11:25:28 -0500

    bump

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ REQUIREMENTS = [
 ]
 
 setuptools.setup(name='inyourface',
-                 version=Version('0.0.7').number,
+                 version=Version('0.0.8').number,
                  description='In Your Face',
                  long_description=open('README.rst').read().strip(),
                  author='Andy Yaco-Mink (@yacomink), Samantha Goldstein (@goldsamantha)',
